c6fcf81fc90e9fc55bb53d4f7a322e43a4d14580
Magda Balun <magda.balun@gmail.com>
2018-10-18 11:25:34 +0200

    Create formHelper file

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
     <form-helper>
-      <h2 slot="title">{{title}}</h2>
-      <p slot="text">I am a paragraph</p>
+      <div slot="form-header">
+        <h3>Form Title</h3>
+        <p>Information about the form</p>
+      </div>
+      <div slot="form-fields">
+        <input type="text" placeholder="name" required/>
+        <input type="text" placeholder="password" required/>
+      </div>
+      <div slot="form-controls">
+        <button v-on:click="handleSubmit">Submit</button>
+      </div>
     </form-helper>
   </div>
 </template>
