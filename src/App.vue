4e83745831c409c239edeea43a5d5c479353e089
Magda Balun <magda.balun@gmail.com>
2018-10-18 11:09:15 +0200

    Create vue files with VueCLI

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <form-helper>
+      <h2 slot="title">{{title}}</h2>
+      <p slot="text">I am a paragraph</p>
+    </form-helper>
+  </div>
+</template>
+
+<script>
+import formHelper from "./components/formHelper.vue";
+
+export default {
+  components: {
+    "form-helper": formHelper
+  },
+  data() {
+    return {
+      title: 'I am a dynamic title'
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style>
+</style>
